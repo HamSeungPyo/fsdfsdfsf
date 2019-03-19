b0992bdf5dab2db51d6ae036cfdad0d578dce544
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-03-19 23:35:38 +0900

    test

---
+++ Assets/Scripts/Item/FireExtManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExtManager : MonoBehaviour
+{
+    public GameObject pin;
+    public GameObject particle;
+    public GameObject trigger;
+    bool bStartingFireExtinguisher=false;
+
+    private void Awake()
+    {
+        StartCoroutine(FireExtinguisherChack());
+    }
+
+    IEnumerator FireExtinguisherChack()
+    {
+        while (true)
+        {
+            if (!pin.activeSelf)
+            {
+                bStartingFireExtinguisher = true;
+            }
+
+            if (bStartingFireExtinguisher)
+                break;
+
+            yield return null;
+        }
+    }
+    public void StartingFireExtinguisher(bool set)
+    {
+        if (bStartingFireExtinguisher)
+        {
+            particle.SetActive(set);
+            trigger.SetActive(set);
+        }
+    }
+    public bool GetPinState()
+    {
+        return bStartingFireExtinguisher;
+    }
+}
