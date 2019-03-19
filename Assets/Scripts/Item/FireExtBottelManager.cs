7067d7bf08bea6626deeed099ad0627cdd2dd374
HamSeungPyo <39762906+HamSeungPyo@users.noreply.github.com>
2019-03-19 23:39:41 +0900

    Revert "dfghdfgh"
    This reverts commit 1fb83ee4a4a3e79c24afa3fc20264ec661e2970b.

---
+++ Assets/Scripts/Item/FireExtBottelManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireExtBottelManager : MonoBehaviour
+{
+    public GameObject renderer;
+    public GameObject effect;
+    public float impactPower = 2;
+    bool bVelocity = false;
+    bool bHolding = false;
+
+    public void ItemHolding(bool set)
+    {
+        bHolding = set;
+    }
+    private void Update()
+    {
+        if (bHolding)
+        {
+            bVelocity = false;
+            return;
+        }
+        Vector3 velocity = GetComponent<Rigidbody>().velocity;
+        float velocityPower = Mathf.Abs(velocity.x) + Mathf.Abs(velocity.y) + Mathf.Abs(velocity.z);
+        if (velocityPower > impactPower)
+        {
+            bVelocity = true;
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Ground" && bVelocity)
+        {
+            StartCoroutine(StartEffect());
+        }
+    }
+
+    IEnumerator StartEffect()
+    {
+        GameObject startEffect = Instantiate(effect, transform.position, Quaternion.identity);
+        renderer.GetComponent<MeshRenderer>().enabled = false;
+        gameObject.GetComponent<CapsuleCollider>().enabled = false;
+        yield return new WaitForSeconds(2f);
+        Destroy(startEffect);
+        Destroy(gameObject);
+    }
+}
